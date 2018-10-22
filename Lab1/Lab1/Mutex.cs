01efe2e1a51e979a5ad914a2272b7e4f260011b8
WildTiger404 <tsarukalex404@gmail.com>
2018-10-23 01:42:36 +0300

    add mutex solution

---
+++ Lab1/Lab1/Mutex.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Mutex
+    {
+        private int ID;
+
+        public Mutex()
+        {
+            this.ID = 0;
+        }
+
+        public void Lock()
+        {
+            int currentID = Thread.CurrentThread.ManagedThreadId;
+            while (Interlocked.CompareExchange(ref ID, currentID, 0) != 0)
+            {
+                Thread.Sleep(10);
+            }
+        }
+
+        public void Unlock()
+        {
+            int currentID = Thread.CurrentThread.ManagedThreadId;
+            Interlocked.CompareExchange(ref ID, 0, currentID);
+        }
+    }
+}
