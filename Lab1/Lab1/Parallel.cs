2ee6520f60ab38acad057f6a22feb923c0a5fa52
WildTiger404 <tsarukalex404@gmail.com>
2018-10-26 19:59:12 +0300

    add parallel solution

---
+++ Lab1/Lab1/Parallel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Lab1
+{
+    static class Parallel
+    {
+        public static void WaitAll(List<TaskQueue.taskDelegate> tasks)
+        {
+            TaskQueue taskQueue = new TaskQueue(5);
+            bool isRunning = true;
+
+            foreach (var del in tasks)
+            {
+                taskQueue.EnqueueTask(del);
+            }
+
+            while (isRunning)
+            {
+                if (taskQueue.AreTasksCompleted())
+                {
+                    Console.WriteLine("All tasks are completed\n\n");
+                    isRunning = false;
+                }
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
