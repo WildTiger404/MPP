4c53c672af7476fa3dde7429714983d13dca6e6f
WildTiger404 <tsarukalex404@gmail.com>
2018-11-06 21:58:14 +0300

    update functionality

--- Lab1/AssemblyInfo/Program.cs
+++ Lab1/AssemblyInfo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,30 @@ namespace AssemblyInfo
     {
         static void Main(string[] args)
         {
+            string path = @"C:\Program Files\Reference Assemblies\Microsoft\Framework\v3.0\ReachFramework.dll";
+            /*try
+            {
+                AssemblyInfo.CheckPathArg(args, out path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetType() +": "+ e.Message);
+                Console.ReadLine();
+                return;
+            }*/
+            try
+            {
+                string[] typesList = AssemblyInfo.LoadandListPublicTypes(path);
+                foreach (string type in typesList)
+                {
+                    Console.WriteLine(type);
+                }
+            }
+            catch (FileLoadException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
